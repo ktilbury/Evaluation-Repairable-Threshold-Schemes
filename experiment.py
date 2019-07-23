f84fb73b588bdb0cfb63f9cd551e2ae60c7fec83
ktilbury <kyle.tilbury21@gmail.com>
2019-07-23 13:30:06 -0400

    Updated experiment - added function to enable easy evaluation of different designs and data frames to store the data

--- experiment.py
+++ experiment.py
@@ -3,6 +3,7 @@
 from repair import *
 
 import random as r
+import pandas as pd
 import time
 
 
@@ -36,82 +37,120 @@ def init_grouped_participants(participants_dic):
                     l.append(other_participant.id_num)
                     participant.grouped_participants[s] = l.copy()
 
+
+def evaluate_design(blocks, availability_probs, fault_models, num_iterations, repair_algorithms):
+    # Instantiate participants and add to dict
+    participants = {}
+    for i, block in enumerate(blocks):
+        p = Participant(i, block)
+        participants.update({i: p})
+    # Initialize extra participant attributes
+    init_intersecting_participants(participants)
+    init_grouped_participants(participants)
+
+    # Initialize dataframe to save results to
+    df = pd.DataFrame(columns=['Algorithm', 'Fault model', 'Availability probability',
+                               'Successful repairs', 'Success total contacted', 'Success average contacted',
+                               'Total success wall clock time', 'Average success wall clock time',
+                               'Total success process time', 'Average success process time',
+                               'Failed repairs', 'Fail total contacted', 'Fail average contacted',
+                               'Total fail wall clock time', 'Average fail wall clock time',
+                               'Total fail process time', 'Average fail process time'])
+
+    # Loop through so many things
+    for repair_algo in repair_algorithms:
+        for fault_model in fault_models:
+            for prob in availability_probs:
+                repair_wall_time = 0.0
+                repair_process_time = 0.0
+                total_success_contacted = 0
+
+                failed_repairs = 0
+                fail_wall_time = 0.0
+                fail_process_time = 0.0
+                total_failed_contacted = 0
+
+                for i in range(num_iterations):
+
+                    participant_to_repair = participants.get((r.sample(participants.keys(), 1)[0]))
+                    participants_copy = participants.copy()  # send copy of dict to algo 1 so can pop from dict for
+                    # permanent fault
+                    participants_copy.pop(
+                        participant_to_repair.id_num)  # remove ptcpnt to repair from dict so don't contact self
+
+                    start_wall_time = time.clock()
+                    start_process_time = time.process_time()
+                    success, participants_contacted = repair_algo(participants_copy, participant_to_repair,
+                                                                  prob, fault_model)
+                    end_wall_time = (time.clock() - start_wall_time)
+                    end_process_time = (time.process_time() - start_process_time)
+
+                    if success:
+                        repair_wall_time += end_wall_time
+                        repair_process_time += end_process_time
+                        total_success_contacted += participants_contacted
+                    elif not success:
+                        failed_repairs += 1
+                        fail_wall_time += end_wall_time
+                        fail_process_time += end_process_time
+                        total_failed_contacted += participants_contacted
+
+                success_repairs = num_iterations - failed_repairs
+
+                results = {}
+                results.update({'Algorithm': str(repair_algo.__name__)})
+                results.update({'Fault model': fault_model})
+                results.update({'Availability probability': prob})
+                results.update({'Fault model': fault_model})
+                if success_repairs > 0:
+                    results.update({'Successful repairs': success_repairs})
+                    results.update({'Success total contacted': total_success_contacted})
+                    results.update({'Success average contacted': (total_success_contacted / success_repairs)})
+                    results.update({'Total success wall clock time': repair_wall_time})
+                    results.update({'Average success wall clock time': (repair_wall_time / success_repairs)})
+                    results.update({'Total success process time': repair_process_time})
+                    results.update({'Average success process time': (repair_process_time / success_repairs)})
+                else:
+                    results.update({'Successful repairs': 0})
+                    results.update({'Success total contacted': None})
+                    results.update({'Success average contacted': None})
+                    results.update({'Total success wall clock time': None})
+                    results.update({'Average success wall clock time': None})
+                    results.update({'Total success process time': None})
+                    results.update({'Average success process time': None})
+
+                if failed_repairs > 0:
+                    results.update({'Failed repairs': failed_repairs})
+                    results.update({'Fail total contacted': total_failed_contacted})
+                    results.update({'Fail average contacted': (total_failed_contacted / failed_repairs)})
+                    results.update({'Total fail wall clock time': fail_wall_time})
+                    results.update({'Average fail wall clock time': (fail_wall_time / failed_repairs)})
+                    results.update({'Total fail process time': fail_process_time})
+                    results.update({'Average fail process time': (fail_process_time / failed_repairs)})
+                else:
+                    results.update({'Failed repairs': 0})
+                    results.update({'Fail total contacted': None})
+                    results.update({'Fail average contacted': None})
+                    results.update({'Total fail wall clock time': None})
+                    results.update({'Average fail wall clock time': None})
+                    results.update({'Total fail process time': None})
+                    results.update({'Average fail process time': None})
+
+                df = df.append(results, ignore_index=True)
+    return df
+
+
 """
 Test algorithm
 """
-blocks = [[0, 1, 3], [0, 2, 6], [0, 4, 5], [1, 2, 4], [1, 5, 6], [2, 3, 5], [3, 4, 6]]
-availability_probabilities = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
-fault_model = "Permanent"
-# fault_model = "Transient"
-num_iterations = 10000
+design_blocks = [[0, 1, 3], [0, 2, 6], [0, 4, 5], [1, 2, 4], [1, 5, 6], [2, 3, 5], [3, 4, 6]]
+avail_probs = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+fault_models_list = ["Permanent", "Transient"]
+num_repair_iterations = 1
+repair_algos_list = [random_participants, stored_intersecting_participants, stored_grouped_participants]
 
-# Instantiate participants and add to dict
-participants = {}
-for i, block in enumerate(blocks):
-    p = Participant(i, block)
-    participants.update({i: p})
-# Initialize extra participant attributes
-init_intersecting_participants(participants)
-init_grouped_participants(participants)
+data = evaluate_design(design_blocks, avail_probs, fault_models_list, num_repair_iterations, repair_algos_list)
 
-# Iterate through availability probabilities: time repairs and count failed repairs at each probability
-for prob in availability_probabilities:
-    repair_wall_time = 0.0
-    repair_process_time = 0.0
-    total_success_contacted = 0
+print(data)
 
-    failed_repairs = 0
-    fail_wall_time = 0.0
-    fail_process_time = 0.0
-    total_failed_contacted = 0
 
-    for i in range(num_iterations):
-
-        participant_to_repair = participants.get((r.sample(participants.keys(), 1)[0]))
-        participants_copy = participants.copy()  # send copy of dict to algo 1 so can pop from dict for permanent fault
-        participants_copy.pop(participant_to_repair.id_num)  # remove ptcpnt to repair from dict so don't contact self
-
-        start_wall_time = time.clock()
-        start_process_time = time.process_time()
-        #Algorithms here
-        # success, participants_contacted = random_participants(participants_copy, participant_to_repair, prob, fault_model)
-        # success, participants_contacted = stored_intersecting_participants(participants_copy, participant_to_repair, prob, fault_model)
-        success, participants_contacted = stored_grouped_participants(participants_copy, participant_to_repair, prob, fault_model)
-        end_wall_time = (time.clock() - start_wall_time)
-        end_process_time = (time.process_time() - start_process_time)
-
-        if success:
-            repair_wall_time += end_wall_time
-            repair_process_time += end_process_time
-            total_success_contacted += participants_contacted
-        elif not success:
-            failed_repairs += 1
-            fail_wall_time += end_wall_time
-            fail_process_time += end_process_time
-            total_failed_contacted += participants_contacted
-
-    success_repairs = num_iterations - failed_repairs
-    print("___________________________________________________________________________________________________________")
-    print("Fault model: " + fault_model)
-    print("Availability probabilty: " + str(prob))
-
-    if success_repairs > 0:
-        print("Successful repairs: " + str(success_repairs))
-        print("Total participants contacted: " + str(total_success_contacted))
-        print("Average participants contacted: " + str(total_success_contacted/success_repairs))
-        print("Total repair wall clock time: " + str(repair_wall_time))
-        print("Average repair wall clock time: " + str(repair_wall_time/success_repairs))
-        print("Total repair process time: " + str(repair_process_time))
-        print("Average repair process time: " + str(repair_process_time/success_repairs))
-    else:
-        print("Successful repairs: 0")
-    if failed_repairs > 0:
-        print("Failed repairs: " + str(failed_repairs))
-        print("Total participants contacted: " + str(total_failed_contacted))
-        print("Average participants contacted: " + str(total_failed_contacted/failed_repairs))
-        print("Total fail wall clock time: " + str(fail_wall_time))
-        print("Average fail wall clock time: " + str(fail_wall_time / failed_repairs))
-        print("Total fail process time: " + str(fail_process_time))
-        print("Average fail process time: " + str(fail_process_time / failed_repairs))
-    else:
-        print("Failed repairs: 0")
